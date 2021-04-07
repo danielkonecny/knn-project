94fec31dd25557b48673c28dda03ea8ca38a491c
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2021-04-07 14:38:51 +0200

    detector potentional BUGFIX set num classes as length of classes_dict.keys
    Signed-off-by: David Sedlák <xsedla1d@stud.fit.vutbr.cz>

--- detector.py
+++ detector.py
@@ -42,7 +42,7 @@ def define_model():
     cfg.SOLVER.STEPS = []        # do not decay learning rate
     cfg.TEST.EVAL_PERIOD = 50
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256   # faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 400 # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(classes_dict.keys()) # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
     # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
     return cfg
 
